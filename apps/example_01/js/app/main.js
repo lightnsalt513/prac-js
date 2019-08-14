dc805966b01808dc00ecab0dc58db1d3a4c7758d
jayDo <jaydo@hivelab.co.kr>
2019-08-14 21:34:32 +0900

    updating

--- apps/example_01/js/app/main.js
+++ apps/example_01/js/app/main.js
@@ -39,6 +39,9 @@
             this.itemCount = 0; // for adding file/folder
             this.selectedItem = null;
             this.contentEditing = null;
+            this.layoutLevelCount = 0;
+            this.levelDepth = '';
+            this.initialLayout = [];
         },
         initAjax : function () {
             var _this = this;
@@ -48,51 +51,36 @@
                 dataType : 'json',
                 success : function (result) {
                     if (!result.data.length) return;
-                    _this.createContainer();
                     _this.createNodes(result.data);
                     _this.afterAjax();
                 }
             });
         },
-        createContainer : function () {
-            var containerElement = [];
-            containerElement.push('<div class="cm-tree__wrap"><ul class="cm-tree__root"></ul></div>');
-            this.obj.append(containerElement.join(''));
-            this.rootElement = this.obj.find(this.opts.rootElement);
-        },
         createNodes : function (data) {
             this.dataArray = data.slice(0); // data cloning
             if (this.opts.startCreate == null) {
-                this.createNodesFunc(this.rootElement, this.dataArray);
+                this.initialLayout.push('<div class="cm-tree__wrap"><ul class="cm-tree__root">');
+                this.createNodesFunc(this.dataArray);
+                this.initialLayout.push('</ul></div>');
+                this.obj.append(this.initialLayout.join(''));
                 this.opts.startCreate = 'started';
             }
         },
-        createNodesFunc : function (parent, data) {
-            var tempText = [],
-                targetParentEl = parent;
+        createNodesFunc : function (data, depth) {
+            var levelDepth = depth || '';
             for (var i = 0; i < data.length; i++) {
-                this.itemCount++;
-                var isFolder = (data[i]['type'] === 'folder') ? true : false;
+                var isFolder = (data[i].hasOwnProperty('children')) ? true : false;
                 if (!isFolder) {
-                    tempText.push('<li class="cm-tree__file"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-item-id="' + data[i]['id'] + '">' + data[i]['name'] + '</button></div></li>');
-                    targetParentEl.append(tempText.join(''));
-                    tempText = [];
+                    this.initialLayout.push('<li class="cm-tree__file"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-level="' + levelDepth + i + '">' + data[i]['name'] + '</button></div></li>');
                 } else {
-                    var folderEl,
-                        childrenWrapEl;
-                    tempText.push('<li class="cm-tree__folder"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-item-id="' + data[i]['id'] + '">' + data[i]['name'] + '</button></div></li>');
-                    targetParentEl.append(tempText.join(''));
-                    folderEl = targetParentEl.children().eq(i);
-                    tempText = [];
-
+                    this.initialLayout.push('<li class="cm-tree__folder"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-level="' + levelDepth + i + '">' + data[i]['name'] + '</button></div>');
                     if (data[i].children.length) {
-                        tempText.push('<ul class="cm-tree__folder-children"></ul>');
-                        folderEl.append(tempText.join(''));
-                        tempText = [];
-    
-                        childrenWrapEl = folderEl.find(this.opts.folderChildrenWrap);
-                        this.createNodesFunc(childrenWrapEl, data[i].children);
+                        var newDepth = levelDepth + i + '_';
+                        this.initialLayout.push('<ul class="cm-tree__folder-children">');
+                        this.createNodesFunc(data[i].children, newDepth);
+                        this.initialLayout.push('</ul>');
                     }
+                    this.initialLayout.push('</li>');
                 }
             }
         },
@@ -121,16 +109,16 @@
         bindEvents : function (type) {
             if (type) {
                 this.btn.on(this.changeEvents('click'), $.proxy(this.onClickBtn, this));
-                this.obj.on(this.changeEvents('changeName'), $.proxy(this.changeNameFunc, this));
+                // this.obj.on(this.changeEvents('changeName'), $.proxy(this.changeNameFunc, this));
                 this.obj.on(this.changeEvents('deleteItem'), $.proxy(this.deleteItemFunc, this));
-                this.obj.on(this.changeEvents('addFileAbove'), $.proxy(this.addFileFunc, this));
-                this.obj.on(this.changeEvents('addFileBelow'), $.proxy(this.addFileFunc, this));
+                // this.obj.on(this.changeEvents('addFileAbove'), $.proxy(this.addFileFunc, this));
+                // this.obj.on(this.changeEvents('addFileBelow'), $.proxy(this.addFileFunc, this));
             } else {
                 this.btn.off(this.changeEvents('click'));
-                this.obj.off(this.changeEvents('changeName'));
+                // this.obj.off(this.changeEvents('changeName'));
                 this.obj.off(this.changeEvents('deleteItem'));
-                this.obj.off(this.changeEvents('addFileAbove'));
-                this.obj.off(this.changeEvents('addFileBelow'));
+                // this.obj.off(this.changeEvents('addFileAbove'));
+                // this.obj.off(this.changeEvents('addFileBelow'));
             }
         },
         onClickBtn : function (e) {
@@ -142,105 +130,131 @@
             target.addClass(this.opts.activeClass);
             this.selectedItem = target;
         },
-        changeNameFunc : function () {
-            if (this.selectedItem == null) return;
-            var target = this.selectedItem,
-                targetId = target.attr('data-item-id'),
-                targetKey = 'id',
-                _this = this,
-                originalContent,
-                newContent;
-            target[0].contentEditable = true;
-            target.on(this.changeEvents('focus'), function () {
-                originalContent = target.html();
-            });
-            target.on(this.changeEvents('focusout'), function () {
-                newContent = target.html();
-                if (originalContent !== newContent) {
-                    _this.changeNameRecursiveFunc(_this.dataArray, targetKey, targetId, newContent);
-                    console.log(_this.dataArray);
-                }
-                target[0].contentEditable = false;
-            });
-            target.focus();
-        },
-        changeNameRecursiveFunc : function (array, targetKey, targetVal, contentVal) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i][targetKey] === targetVal) {
-                    array[i]['name'] = contentVal;
-                    break; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
-                } else if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
-                    this.changeNameRecursiveFunc(array[i].children, targetKey, targetVal, contentVal);
-                }
-            }
-        },
+        // changeNameFunc : function () {
+        //     if (this.selectedItem == null) return;
+        //     var target = this.selectedItem,
+        //         targetId = target.attr('data-item-id'),
+        //         targetKey = 'id',
+        //         _this = this,
+        //         originalContent,
+        //         newContent;
+        //     target[0].contentEditable = true;
+        //     target.on(this.changeEvents('focus'), function () {
+        //         originalContent = target.html();
+        //     });
+        //     target.on(this.changeEvents('focusout'), function () {
+        //         newContent = target.html();
+        //         if (originalContent !== newContent) {
+        //             _this.changeNameRecursiveFunc(_this.dataArray, targetKey, targetId, newContent);
+        //             console.log(_this.dataArray);
+        //         }
+        //         target[0].contentEditable = false;
+        //     });
+        //     target.focus();
+        // },
+        // changeNameRecursiveFunc : function (array, targetKey, targetVal, contentVal) {
+        //     for (var i = 0; i < array.length; i++) {
+        //         if (array[i][targetKey] === targetVal) {
+        //             array[i]['name'] = contentVal;
+        //             console.log('before break');
+        //             break; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
+        //         } else if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
+        //             console.log('looping');
+        //             this.changeNameRecursiveFunc(array[i].children, targetKey, targetVal, contentVal);
+        //         }
+        //     }
+        // },
         deleteItemFunc : function () {
             if (this.selectedItem == null) return;
             var target = this.selectedItem, 
-                targetId = target.attr('data-item-id'),
-                targetKey = 'id';
+                targetData = target.attr('data-level'),
+                targetLevelArray = targetData.split('_'),
+                targetIndex = null,
+                indexGroupArray = [],
+                indexGroup = '';
+                
+            // for (var i = 0; i < targetLevelArray.length - 1; i++) {
+            //     indexGroupArray.push('[' + targetLevelArray[i] + ']');
+            //     if (i === targetLevelArray.length - 1) {
+            //         targetIndex = i;
+            //     }
+            // }
+            // indexGroup = '1';
+            // var testing = Function(' return this.dataArray[indexGroup] ');
+            // // testing();
+            // console.log(testing());
+            // eval((this.dataArray + indexGroup)).splice(targetIndex, 1)
 
-            this.deleteArrRecursiveFunc(this.dataArray, targetKey, targetId);
-            this.selectedItem.closest('li').remove();
-            this.afterAjax();
-            console.log(this.dataArray);
-        },
-        deleteArrRecursiveFunc : function (array, targetKey, targetVal) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i][targetKey] === targetVal) {
-                    array.splice(i, 1);
-                    break; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
-                } else if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
-                    this.deleteArrRecursiveFunc(array[i].children, targetKey, targetVal);
-                }
-            }
-        },
-        addFileFunc : function (e) {
-            if (this.selectedItem == null) return;
-            var baseTarget = this.selectedItem,
-                baseTargetId = baseTarget.attr('data-item-id'),
-                baseTargetKey = 'id',
-                position = (function() {
-                    if (e.type === 'addFileAbove') {
-                        return 'above';
-                    } else if (e.type === 'addFileBelow') {
-                        return'below';
-                    }
-                })(),
-                tempText = [];
+            function recursiveFunc (targetLevelArray, ) {
+                
+            };
+            
 
-            this.itemCount++;
-            this.addFileRecursiveFunc(this.dataArray, baseTargetKey, baseTargetId, position); // above / below
-            tempText.push('<li class="cm-tree__file"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-item-id="item_' + this.itemCount + '">' + 'NEW FILE' + '</button></div></li>');
-            if (position === 'above') {
-                this.selectedItem.closest('li').before(tempText.join(''));
-            } else if (position === 'below') {
-                this.selectedItem.closest('li').after(tempText.join(''));
-            }
-            this.afterAjax();
-            console.log(this.dataArray);
+            // check how many depths
+            // find out all depth and index
+            // remove from original array
+            
+
+            // this.selectedItem.closest('li').remove();
+            // this.afterAjax();
+            // console.log(this.dataArray);
         },
-        addFileRecursiveFunc : function (array, targetKey, targetVal, position) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i][targetKey] === targetVal) {
-                    var newItem = {
-                        "name" : "NEW FILE",
-                        "id" : "item_" + this.itemCount,
-                        "type" : "file"
-                    };
-                    if (position === 'above') {
-                        array.splice(i, 0, newItem);
-                    } else if (position === 'below') {
-                        array.splice(i+1, 0, newItem);
-                    }
-                    return; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
-                } else {
-                    if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
-                        this.addFileRecursiveFunc(array[i].children, targetKey, targetVal, position);
-                    }
-                }
-            }
-        }
+        // deleteArrRecursiveFunc : function (array, targetKey, targetVal) {
+        //     for (var i = 0; i < array.length; i++) {
+        //         if (array[i][targetKey] === targetVal) {
+        //             array.splice(i, 1);
+        //             break; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
+        //         } else if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
+        //             this.deleteArrRecursiveFunc(array[i].children, targetKey, targetVal);
+        //         }
+        //     }
+        // },
+        // addFileFunc : function (e) {
+        //     if (this.selectedItem == null) return;
+        //     var baseTarget = this.selectedItem,
+        //         baseTargetId = baseTarget.attr('data-item-id'),
+        //         baseTargetKey = 'id',
+        //         position = (function() {
+        //             if (e.type === 'addFileAbove') {
+        //                 return 'above';
+        //             } else if (e.type === 'addFileBelow') {
+        //                 return'below';
+        //             }
+        //         })(),
+        //         layout = [];
+
+        //     this.itemCount++;
+        //     this.addFileRecursiveFunc(this.dataArray, baseTargetKey, baseTargetId, position); // above / below
+        //     layout.push('<li class="cm-tree__file"><div class="cm-tree__content"><button type="button" class="cm-tree-btn" data-item-id="item_' + this.itemCount + '">' + 'NEW FILE' + '</button></div></li>');
+        //     if (position === 'above') {
+        //         this.selectedItem.closest('li').before(layout.join(''));
+        //     } else if (position === 'below') {
+        //         this.selectedItem.closest('li').after(layout.join(''));
+        //     }
+        //     this.afterAjax();
+        //     console.log(this.dataArray);
+        // },
+        // addFileRecursiveFunc : function (array, targetKey, targetVal, position) {
+        //     for (var i = 0; i < array.length; i++) {
+        //         if (array[i][targetKey] === targetVal) {
+        //             var newItem = {
+        //                 "name" : "NEW FILE",
+        //                 "id" : "item_" + this.itemCount,
+        //                 "type" : "file"
+        //             };
+        //             if (position === 'above') {
+        //                 array.splice(i, 0, newItem);
+        //             } else if (position === 'below') {
+        //                 array.splice(i+1, 0, newItem);
+        //             }
+        //             return; // loop 여기서 끝나도록 다시 개발 필요 (break? return?)
+        //         } else {
+        //             if (array[i]['children'] !== undefined && array[i]['children'].length > 0) {
+        //                 this.addFileRecursiveFunc(array[i].children, targetKey, targetVal, position);
+        //             }
+        //         }
+        //     }
+        // }
     };
 
     $(function () {
